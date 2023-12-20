7075da414dce42e7d8c6b941d95e40828ce6c506
Nasrul Wahabi <wahabinasrul@gmail.com>
2023-12-20 22:42:14 +0700

    Add quiz app start screen and logo image

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobile/udemy_max/dice_roller/gradient_container.dart';
+import 'package:flutter_mobile/udemy_max/quiz_app/start_screen.dart';
 
 void main(List<String> args) {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: GradientContainer(
-          colorTop: Colors.red,
-          colorBottom: Colors.blue,
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            colors: [
+              Colors.red,
+              Colors.blue,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          )),
+          child: const StartScreen(),
         ),
+
+        // body: GradientContainer(
+        //   colorTop: Colors.red,
+        //   colorBottom: Colors.blue,
+        // ),
       ),
     ),
   );
---
+++ lib/udemy_max/quiz_app/start_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min, // Column will take the minimum space
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.black, backgroundColor: const Color.fromARGB(255, 255, 218, 5),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 30,
+                vertical: 15,
+              ),
+              shape: const StadiumBorder(),
+            ),icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
+          )
+        ],
+      ),
+    );
+  }
+}
