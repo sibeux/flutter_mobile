17334bd9ecab7941597be8dd9f317295b86cc068
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-01-20 00:26:34 +0700

    Add VSCode settings and update build.gradle and theme in Expenses app

--- lib/udemy_max/expenses_track/main_expenses_app.dart
+++ lib/udemy_max/expenses_track/main_expenses_app.dart
@@ -5,38 +5,61 @@ import 'package:flutter_mobile/udemy_max/expenses_track/widgets/expenses.dart';
 var kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromRGBO(69, 180, 73, 1));
 
+var kColorSchemeDark = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 69, 76, 180),
+  brightness: Brightness.dark,
+);
+
 class MainExpensesApp extends StatelessWidget {
   const MainExpensesApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kColorSchemeDark,
+        cardTheme: const CardTheme().copyWith(
+          color: kColorSchemeDark.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorSchemeDark.primaryContainer,
+            foregroundColor: kColorSchemeDark.onPrimaryContainer,
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
-          useMaterial3: true,
-          colorScheme: kColorScheme,
-          appBarTheme: const AppBarTheme().copyWith(
-            backgroundColor: kColorScheme.onPrimaryContainer,
-            foregroundColor: kColorScheme.primaryContainer,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
           ),
-          cardTheme: const CardTheme().copyWith(
-            color: kColorScheme.secondaryContainer,
-            margin: const EdgeInsets.symmetric(
-              horizontal: 16,
-              vertical: 8,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 14,
+              ),
             ),
-          ),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: kColorScheme.primaryContainer,
-            ),
-          ),
-          textTheme: ThemeData().textTheme.copyWith(
-                titleLarge: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: kColorScheme.onSecondaryContainer,
-                  fontSize: 14,
-                ),
-              )),
+      ),
+      // themeMode: ThemeMode.sytems, => default
+      themeMode: ThemeMode.dark,
       home: const Expenses(),
     );
   }
--- lib/udemy_max/expenses_track/models/expense.dart
+++ lib/udemy_max/expenses_track/models/expense.dart
@@ -31,3 +31,5 @@ class Expense {
 
   String get formattedDate => formatter.format(date);
 }
+
+
