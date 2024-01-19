18ca68db660cb62b9c5b65891ee37f5d29f0a83b
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-01-20 01:32:06 +0700

    Add Flutter Services package and lock orientation to portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: unused_import
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_mobile/course_kopianan/main_anan.dart';
 import 'package:flutter_mobile/udemy_max/dice_roller/gradient_container.dart';
 import 'package:flutter_mobile/udemy_max/dice_roller/main_dice_roller.dart';
@@ -9,7 +10,15 @@ import 'package:flutter_mobile/udemy_max/quiz_app/main_quiz_app.dart';
 import 'package:flutter_mobile/udemy_max/quiz_app/start_screen.dart';
 
 void main(List<String> args) {
-  // tinggal ganti MainApp-nya 
-  runApp(const MainExpensesApp()
-  );
+
+  // lock orientation to portrait
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+  //     .then((fn) {
+    // tinggal ganti MainApp-nya
+  //   runApp(const MainExpensesApp());
+  // });
+
+    // tinggal ganti MainApp-nya
+    runApp(const MainExpensesApp());
 }
--- lib/udemy_max/expenses_track/main_expenses_app.dart
+++ lib/udemy_max/expenses_track/main_expenses_app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 import 'package:flutter_mobile/udemy_max/expenses_track/widgets/expenses.dart';
 
 var kColorScheme =
