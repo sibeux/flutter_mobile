d34f8d855335c3f283a91cc6055aa030a4bd7bb7
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-01-23 01:06:57 +0700

    Refactor expense form layout and add safe area handling

--- lib/udemy_max/expenses_track/widgets/expenses.dart
+++ lib/udemy_max/expenses_track/widgets/expenses.dart
@@ -29,6 +29,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay(BuildContext context) {
     showModalBottomSheet(
+      useSafeArea: true,
       isScrollControlled: true,
       context: context,
       builder: (ctx) {
