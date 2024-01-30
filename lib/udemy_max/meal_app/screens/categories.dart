cb31cd4aafb942d4fe10bb81a3a54a6808bdaa8e
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-01-30 11:13:06 +0700

    Refactor _selectCategory method to filter meals by category

--- lib/udemy_max/meal_app/screens/categories.dart
+++ lib/udemy_max/meal_app/screens/categories.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobile/udemy_max/meal_app/data/dummy_data.dart';
+import 'package:flutter_mobile/udemy_max/meal_app/models/category.dart';
 import 'package:flutter_mobile/udemy_max/meal_app/screens/meals.dart';
 import 'package:flutter_mobile/udemy_max/meal_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
 
-  void _selectCategory(BuildContext context, String id, String title) {
+  void _selectCategory(
+      BuildContext context, String id, String title, Category category) {
+    final filteredMeal =
+        dummyMeals.where((meal) => meal.categories.contains(category.id)).toList();
+
     // pushNamed() is used to navigate to a named route
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (context) => const MealsScreen(
-          title: 'Some title',
-          meals: [],
+        builder: (context) => MealsScreen(
+          title: category.title,
+          meals: filteredMeal,
         ),
       ),
     );
@@ -38,7 +43,7 @@ class CategoriesScreen extends StatelessWidget {
             CategoryGridItem(
               category: category,
               onSelectCategory: () {
-                _selectCategory(context, category.id, category.title);
+                _selectCategory(context, category.id, category.title, category);
               },
             ),
         ],
