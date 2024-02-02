3483635cf3bf9e56f5d5eeca58c0780caa20737c
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-02-02 07:22:18 +0700

    Refactor filters screen and add new filter options

--- lib/udemy_max/meal_app/screens/filters.dart
+++ lib/udemy_max/meal_app/screens/filters.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobile/udemy_max/meal_app/screens/tabs.dart';
-import 'package:flutter_mobile/udemy_max/meal_app/widgets/main_drawer.dart';
+// import 'package:flutter_mobile/udemy_max/meal_app/screens/tabs.dart';
+// import 'package:flutter_mobile/udemy_max/meal_app/widgets/main_drawer.dart';
 
 class FiltersScreen extends StatefulWidget {
   const FiltersScreen({
@@ -13,6 +13,9 @@ class FiltersScreen extends StatefulWidget {
 
 class _FiltersScreenState extends State<FiltersScreen> {
   var _glutenFreeFilterSet = false;
+  var _lactoseFreeFilterSet = false;
+  var _vegetarianFreeFilterSet = false;
+  var _veganFreeFilterSet = false;
 
   @override
   Widget build(BuildContext context) {
@@ -20,46 +23,74 @@ class _FiltersScreenState extends State<FiltersScreen> {
       appBar: AppBar(
         title: const Text('Your Filters'),
       ),
-      drawer: MainDrawer(
-        onSelectScreen: (identifier) {
-          Navigator.of(context).pop();
-          if (identifier == 'meals') {
-            Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) {
-                return const TabsScreen();
-              }),
-            );
-          }
-        },
-      ),
+      // drawer: MainDrawer(
+      //   onSelectScreen: (identifier) {
+      //     Navigator.of(context).pop();
+      //     if (identifier == 'meals') {
+      //       Navigator.of(context).pushReplacement(
+      //         MaterialPageRoute(builder: (context) {
+      //           return const TabsScreen();
+      //         }),
+      //       );
+      //     }
+      //   },
+      // ),
       body: Column(
         children: [
-          SwitchListTile(
-            value: _glutenFreeFilterSet,
-            onChanged: (isChecked) {
-              setState(() {
-                _glutenFreeFilterSet = isChecked;
-              });
-            },
-            title: Text(
-              'Gluten-free',
-              style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
-                  ),
-            ),
-            subtitle: Text(
-              'Only include gluten-free meals.',
-              style: Theme.of(context).textTheme.labelMedium!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
-                  ),
-            ),
-            activeColor: Theme.of(context).colorScheme.tertiary,
-            contentPadding: const EdgeInsets.only(
-              left: 34,
-              right: 22,
-            ),
-          ),
+          listFilters(context,
+              title: 'Gluten-free',
+              subtitle: 'Only include gluten-free meals.',
+              value: _glutenFreeFilterSet),
+          listFilters(context,
+              title: 'Lactose-free',
+              subtitle: 'Only include lactose-free meals.',
+              value: _lactoseFreeFilterSet),
+          listFilters(context,
+              title: 'Vegetarian-free',
+              subtitle: 'Only include vegetarian-free meals.',
+              value: _vegetarianFreeFilterSet),
+          listFilters(context,
+              title: 'Vegan-free',
+              subtitle: 'Only include vegan-free meals.',
+              value: _veganFreeFilterSet),
         ],
+      ),
+    );
+  }
+
+  SwitchListTile listFilters(BuildContext context,
+      {required String title, required String subtitle, required bool value}) {
+    return SwitchListTile(
+      value: value,
+      onChanged: (isChecked) {
+        setState(() {
+          if (title == 'Gluten-free') {
+            _glutenFreeFilterSet = isChecked;
+          } else if (title == 'Lactose-free') {
+            _lactoseFreeFilterSet = isChecked;
+          } else if (title == 'Vegetarian-free') {
+            _vegetarianFreeFilterSet = isChecked;
+          } else if (title == 'Vegan-free') {
+            _veganFreeFilterSet = isChecked;
+          }
+        });
+      },
+      title: Text(
+        title,
+        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+      ),
+      subtitle: Text(
+        subtitle,
+        style: Theme.of(context).textTheme.labelMedium!.copyWith(
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+      ),
+      activeColor: Theme.of(context).colorScheme.tertiary,
+      contentPadding: const EdgeInsets.only(
+        left: 34,
+        right: 22,
       ),
     );
   }
