ff6b04b0d8eb110d6067370b0a4f79d5d862be9b
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-02-04 11:28:25 +0700

    Refactor filters and add filteredMealsProvider

--- lib/udemy_max/meal_app/providers/filters_provider.dart
+++ lib/udemy_max/meal_app/providers/filters_provider.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_mobile/udemy_max/meal_app/providers/meals_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 enum Filter {
@@ -7,13 +8,14 @@ enum Filter {
   Vegan,
 }
 
-class FiltersNotifier extends StateNotifier<Map<Filter, bool>>{
-  FiltersNotifier() : super({
-    Filter.GlutenFree: false,
-    Filter.LactoseFree: false,
-    Filter.Vegetarian: false,
-    Filter.Vegan: false,
-  });
+class FiltersNotifier extends StateNotifier<Map<Filter, bool>> {
+  FiltersNotifier()
+      : super({
+          Filter.GlutenFree: false,
+          Filter.LactoseFree: false,
+          Filter.Vegetarian: false,
+          Filter.Vegan: false,
+        });
 
   void setFilters(Map<Filter, bool> chosenFilters) {
     state = chosenFilters;
@@ -27,6 +29,30 @@ class FiltersNotifier extends StateNotifier<Map<Filter, bool>>{
   }
 }
 
-final filtersProvider = StateNotifierProvider<FiltersNotifier, Map<Filter, bool>>(
+final filtersProvider =
+    StateNotifierProvider<FiltersNotifier, Map<Filter, bool>>(
   (ref) => FiltersNotifier(),
 );
+
+final filteredMealsProvider = Provider((ref) {
+  final meals = ref.watch(mealsProvider);
+  final activeFilters = ref.watch(filtersProvider);
+
+  return meals.where(
+    (meal) {
+      if (activeFilters[Filter.GlutenFree]! && !meal.isGlutenFree) {
+        return false;
+      }
+      if (activeFilters[Filter.LactoseFree]! && !meal.isLactoseFree) {
+        return false;
+      }
+      if (activeFilters[Filter.Vegetarian]! && !meal.isVegetarian) {
+        return false;
+      }
+      if (activeFilters[Filter.Vegan]! && !meal.isVegetarian) {
+        return false;
+      }
+      return true;
+    },
+  ).toList();
+});
--- lib/udemy_max/meal_app/screens/tabs.dart
+++ lib/udemy_max/meal_app/screens/tabs.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobile/udemy_max/meal_app/providers/favorites_provider.dart';
 import 'package:flutter_mobile/udemy_max/meal_app/providers/filters_provider.dart';
-import 'package:flutter_mobile/udemy_max/meal_app/providers/meals_provider.dart';
 import 'package:flutter_mobile/udemy_max/meal_app/screens/categories.dart';
 import 'package:flutter_mobile/udemy_max/meal_app/screens/filters.dart';
 import 'package:flutter_mobile/udemy_max/meal_app/screens/meals.dart';
@@ -46,25 +45,7 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final meals = ref.watch(mealsProvider);
-    final activeFilters = ref.watch(filtersProvider);
-    final availableMeals = meals.where(
-      (meal) {
-        if (activeFilters[Filter.GlutenFree]! && !meal.isGlutenFree) {
-          return false;
-        }
-        if (activeFilters[Filter.LactoseFree]! && !meal.isLactoseFree) {
-          return false;
-        }
-        if (activeFilters[Filter.Vegetarian]! && !meal.isVegetarian) {
-          return false;
-        }
-        if (activeFilters[Filter.Vegan]! && !meal.isVegetarian) {
-          return false;
-        }
-        return true;
-      },
-    ).toList();
+    final availableMeals = ref.watch(filteredMealsProvider);
 
     Widget activePage = CategoriesScreen(
       availableMeals: availableMeals,
