ad81e62fecae83ab746b7175db3cf66a7a89a97d
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-02-05 06:35:17 +0700

    Refactor theme and add category cards

--- lib/SiHALAL-ecommerce-app/main_sihalal_app.dart
+++ lib/SiHALAL-ecommerce-app/main_sihalal_app.dart
@@ -1,16 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_mobile/SiHALAL-ecommerce-app/screens/dashboard.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 var kPrimaryColor = const Color.fromARGB(255, 61, 195, 163);
-
-final theme = ThemeData(
-  useMaterial3: true,
-  textTheme: GoogleFonts.montserratAlternatesTextTheme(),
-  colorSchemeSeed: kPrimaryColor,
-  colorScheme: ColorScheme.fromSeed(seedColor: kPrimaryColor),
-);
 
 class MainSihalalApp extends StatelessWidget {
   const MainSihalalApp({Key? key}) : super(key: key);
@@ -22,9 +14,8 @@ class MainSihalalApp extends StatelessWidget {
         statusBarColor: kPrimaryColor,
       ),
     );
-    return MaterialApp(
-      theme: theme,
-      home: const DashboardScreen(),
+    return const MaterialApp(
+      home: DashboardScreen(),
     );
   }
 }
