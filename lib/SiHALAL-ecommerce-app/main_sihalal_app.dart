292d019eff2986fa63b6b9a262b250883a991cb8
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-02-05 05:35:50 +0700

    Update theme in main_sihalal_app.dart

--- lib/SiHALAL-ecommerce-app/main_sihalal_app.dart
+++ lib/SiHALAL-ecommerce-app/main_sihalal_app.dart
@@ -6,10 +6,11 @@ import 'package:google_fonts/google_fonts.dart';
 var kPrimaryColor = const Color.fromARGB(255, 61, 195, 163);
 
 final theme = ThemeData(
-    useMaterial3: true,
-    textTheme: GoogleFonts.montserratAlternatesTextTheme(),
-    colorSchemeSeed: kPrimaryColor,
-    colorScheme: ColorScheme.fromSeed(seedColor: kPrimaryColor));
+  useMaterial3: true,
+  textTheme: GoogleFonts.montserratAlternatesTextTheme(),
+  colorSchemeSeed: kPrimaryColor,
+  colorScheme: ColorScheme.fromSeed(seedColor: kPrimaryColor),
+);
 
 class MainSihalalApp extends StatelessWidget {
   const MainSihalalApp({Key? key}) : super(key: key);
