8d072caad57421e8d810bf271dad513200415b35
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-02-15 21:38:48 +0700

    Add form fields for quantity and category selection

--- lib/udemy_max/shopping_list/widgets/new_item.dart
+++ lib/udemy_max/shopping_list/widgets/new_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobile/udemy_max/shopping_list/data/categories.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -27,6 +28,39 @@ class _NewItemState extends State<NewItem> {
               validator: (value) {
                 return 'demo...';
               },
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      label: Text('Quantity'),
+                    ),
+                    initialValue: '1',
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: DropdownButtonFormField(items: [
+                    for (final category in categories.entries)
+                      DropdownMenuItem(
+                        value: category.value,
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 16,
+                              height: 16,
+                              color: category.value.color,
+                            ),
+                            const SizedBox(),
+                            Text(category.value.title),
+                          ],
+                        ),
+                      )
+                  ], onChanged: (value) {}),
+                )
+              ],
             ),
           ],
         )),
