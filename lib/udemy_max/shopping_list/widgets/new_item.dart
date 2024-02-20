762d7813bba25b03bdd6cc33cd3f59ffe8eb68b7
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-02-20 15:02:32 +0700

    Add form validation and save functionality to NewItem widget

--- lib/udemy_max/shopping_list/widgets/new_item.dart
+++ lib/udemy_max/shopping_list/widgets/new_item.dart
@@ -9,6 +9,12 @@ class NewItem extends StatefulWidget {
 }
 
 class _NewItemState extends State<NewItem> {
+  final _formKey = GlobalKey<FormState>();
+
+  void _saveItem() {
+    _formKey.currentState!.validate();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,81 +24,85 @@ class _NewItemState extends State<NewItem> {
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: Form(
+            key: _formKey,
             child: Column(
-          children: [
-            TextFormField(
-              maxLength: 50,
-              decoration: const InputDecoration(
-                label: Text('Name'),
-              ),
-              validator: (value) {
-                if (value == null ||
-                    value.isEmpty ||
-                    value.trim().length <= 1 ||
-                    value.trim().length > 50) {
-                  return 'Must be between 1 and 50 characters long';
-                }
-                return null;
-              },
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                Expanded(
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      label: Text('Quantity'),
-                    ),
-                    initialValue: '1',
-                    validator: (value) {
-                      if (value == null ||
-                          value.isEmpty ||
-                          int.tryParse(value) == null ||
-                          int.tryParse(value)! <= 0) {
-                        return 'Must be a valid positive number';
-                      }
-                      return null;
-                    },
+                TextFormField(
+                  maxLength: 50,
+                  decoration: const InputDecoration(
+                    label: Text('Name'),
                   ),
+                  validator: (value) {
+                    if (value == null ||
+                        value.isEmpty ||
+                        value.trim().length <= 1 ||
+                        value.trim().length > 50) {
+                      return 'Must be between 1 and 50 characters long';
+                    }
+                    return null;
+                  },
                 ),
-                const SizedBox(width: 8),
-                Expanded(
-                  child: DropdownButtonFormField(items: [
-                    for (final category in categories.entries)
-                      DropdownMenuItem(
-                        value: category.value,
-                        child: Row(
-                          children: [
-                            Container(
-                              width: 16,
-                              height: 16,
-                              color: category.value.color,
-                            ),
-                            const SizedBox(),
-                            Text(category.value.title),
-                          ],
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          label: Text('Quantity'),
                         ),
-                      )
-                  ], onChanged: (value) {}),
-                )
-              ],
-            ),
-            const SizedBox(height: 12),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton(
-                  onPressed: () {},
-                  child: const Text('reset'),
+                        keyboardType: TextInputType.number,
+                        initialValue: '1',
+                        validator: (value) {
+                          if (value == null ||
+                              value.isEmpty ||
+                              int.tryParse(value) == null ||
+                              int.tryParse(value)! <= 0) {
+                            return 'Must be a valid positive number';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    Expanded(
+                      child: DropdownButtonFormField(items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: category.value.color,
+                                ),
+                                const SizedBox(),
+                                Text(category.value.title),
+                              ],
+                            ),
+                          )
+                      ], onChanged: (value) {}),
+                    )
+                  ],
                 ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Add Item'),
+                const SizedBox(height: 12),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        _formKey.currentState!.reset();
+                      },
+                      child: const Text('reset'),
+                    ),
+                    ElevatedButton(
+                      onPressed: _saveItem,
+                      child: const Text('Add Item'),
+                    )
+                  ],
                 )
               ],
-            )
-          ],
-        )),
+            )),
       ),
     );
   }
