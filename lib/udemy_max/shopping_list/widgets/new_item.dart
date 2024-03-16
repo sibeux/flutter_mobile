b1f3886dda86078d75a1ac5e407eea4d33cd53f8
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-03-16 23:50:43 +0700

    Add http package and update saveItem method

--- lib/udemy_max/shopping_list/widgets/new_item.dart
+++ lib/udemy_max/shopping_list/widgets/new_item.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_mobile/udemy_max/shopping_list/data/categories.dart';
 import 'package:flutter_mobile/udemy_max/shopping_list/models/category.dart';
-import 'package:flutter_mobile/udemy_max/shopping_list/models/grocery_item.dart';
+// import 'package:flutter_mobile/udemy_max/shopping_list/models/grocery_item.dart';
+import 'package:http/http.dart' as http;
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -16,17 +19,29 @@ class _NewItemState extends State<NewItem> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      Navigator.of(context).pop(
-        GroceryItem(
-          id: DateTime.now().toString(),
-          name: _enteredName,
-          quantity: _enteredQuantity,
-          category: _selectedCategory,
+      final url = Uri.https("flutter-app-sibeux-default-rtdb.firebaseio.com",
+          "/shopping_list.json");
+      await http.post(
+        url,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: json.encode(
+          {
+            "name": _enteredName,
+            "quantity": _enteredQuantity,
+            "category": _selectedCategory.title,
+          },
         ),
       );
+
+      if (!context.mounted) {
+        return;
+      }
+      Navigator.of(context).pop();
     }
   }
 
