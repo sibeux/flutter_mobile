4df0e30608f38cb41e53aa1e0913d82f6cb44bcc
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-03-17 15:14:10 +0700

    Add error handling for failed API requests

--- lib/udemy_max/shopping_list/widgets/grocery_list.dart
+++ lib/udemy_max/shopping_list/widgets/grocery_list.dart
@@ -16,6 +16,7 @@ class GroceryList extends StatefulWidget {
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
   var isLoading = true;
+  String? _error;
 
   @override
   void initState() {
@@ -27,6 +28,20 @@ class _GroceryListState extends State<GroceryList> {
     final url = Uri.https("flutter-app-sibeux-default-rtdb.firebaseio.com",
         "/shopping_list.json");
     final response = await http.get(url);
+
+    if (response.statusCode >= 400) {
+      setState(() {
+        _error = 'Failed to load items';
+      });
+    }
+
+    if (response.body == 'null') {
+      setState(() {
+        isLoading = false;
+      });
+      return;
+    }
+
     final Map<String, dynamic> listData = json.decode(response.body);
     final List<GroceryItem> loadedItems = [];
     for (final item in listData.entries) {
@@ -66,10 +81,22 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
-  void _removeItem(GroceryItem item) {
+  void _removeItem(GroceryItem item) async {
+    final index = _groceryItems.indexOf(item);
     setState(() {
       _groceryItems.remove(item);
     });
+
+    final url = Uri.https("flutter-app-sibeux-default-rtdb.firebaseio.com",
+        "/shopping_list/${item.id}.json");
+
+    final response = await http.delete(url);
+
+    if (response.statusCode >= 400) {
+      setState(() {
+        _groceryItems.insert(index, item);
+      });
+    }
   }
 
   @override
@@ -102,6 +129,12 @@ class _GroceryListState extends State<GroceryList> {
             trailing: Text('${_groceryItems[index].quantity}x'),
           ),
         ),
+      );
+    }
+
+    if (_error != null) {
+      content = Center(
+        child: Text(_error!),
       );
     }
 
