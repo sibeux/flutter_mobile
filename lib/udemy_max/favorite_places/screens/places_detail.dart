0d528994559556c36cd3f5e6893d478dea93e581
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-03-24 00:06:39 +0700

    Add location image and address to place detail screen and place list item

--- lib/udemy_max/favorite_places/screens/places_detail.dart
+++ lib/udemy_max/favorite_places/screens/places_detail.dart
@@ -6,6 +6,10 @@ class PlaceDetailScreen extends StatelessWidget {
 
   final Place place;
 
+  String get locationImage {
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=${place.location.latitude},${place.location.longitude}&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C${place.location.latitude},${place.location.longitude}&key=AIzaSyD-j_GLs2j6os2dLQw5KZsLk6vHTwgWojI';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +24,37 @@ class PlaceDetailScreen extends StatelessWidget {
             height: double.infinity,
             fit: BoxFit.cover,
           ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Column(children: [
+              CircleAvatar(
+                radius: 70,
+                backgroundImage: NetworkImage(locationImage),
+              ),
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(
+                  vertical: 16,
+                  horizontal: 24,
+                ),
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.transparent, Colors.black54],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                ),
+                child: Text(
+                  place.location.address,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground),
+                ),
+              )
+            ]),
+          )
         ],
       ),
     );
