2646905fb9d888270cff6809cd6d831e92e7b3dd
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-04-02 01:37:20 +0700

    Add music detail screen UI

--- lib/unicorn-app/music-player-app/screens/music_detail_screen.dart
+++ lib/unicorn-app/music-player-app/screens/music_detail_screen.dart
@@ -1,17 +1,181 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_mobile/unicorn-app/music-player-app/models/music.dart';
 
-class MusicDetailScreen extends StatelessWidget {
+class MusicDetailScreen extends StatefulWidget {
   const MusicDetailScreen({super.key, required this.music});
 
   final Music music;
 
   @override
+  State<MusicDetailScreen> createState() => _MusicDetailScreenState();
+}
+
+class _MusicDetailScreenState extends State<MusicDetailScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(music.title),
-      ),
+    return Stack(
+      children: [
+        SizedBox(
+          height: double.infinity,
+          width: double.infinity,
+          child: ImageFiltered(
+              imageFilter: ImageFilter.blur(
+                sigmaY: 35,
+                sigmaX: 35,
+              ),
+              child: Image(
+                fit: BoxFit.cover,
+                filterQuality: FilterQuality.low,
+                color: Colors.black.withOpacity(0.6),
+                colorBlendMode: BlendMode.darken,
+                image: NetworkImage(
+                  widget.music.cover,
+                  scale: 5,
+                ),
+              )),
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            leading: IconButton(
+              icon: const Icon(
+                Icons.keyboard_arrow_down,
+                size: 35,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            actions: [
+              IconButton(
+                icon: const Icon(
+                  Icons.more_vert,
+                  size: 35,
+                  color: Colors.white,
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          body: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  width: 340,
+                  height: 350,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    image: DecorationImage(
+                      filterQuality: FilterQuality.low,
+                      image: NetworkImage(widget.music.cover),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    widget.music.title,
+                    style: TextStyle(
+                      overflow: TextOverflow.ellipsis,
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight: FontWeight.values[5],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  widget.music.artist,
+                  style: const TextStyle(
+                    overflow: TextOverflow.ellipsis,
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        icon: const Icon(
+                          Icons.repeat,
+                          size: 30,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      IconButton(
+                        icon: const Icon(
+                          Icons.skip_previous,
+                          size: 30,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      IconButton(
+                        icon: const Icon(
+                          Icons.play_circle_fill,
+                          size: 60,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      IconButton(
+                        icon: const Icon(
+                          Icons.skip_next,
+                          size: 30,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      IconButton(
+                        icon: const Icon(
+                          Icons.list_alt,
+                          size: 30,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 }
