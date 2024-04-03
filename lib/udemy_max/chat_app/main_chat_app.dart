30f0d3a8f00d5d5759510bbf37b0763bf04c51ad
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-04-03 20:53:31 +0700

    Add chat and splash screens

--- lib/udemy_max/chat_app/main_chat_app.dart
+++ lib/udemy_max/chat_app/main_chat_app.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobile/udemy_max/chat_app/screens/auth.dart';
+import 'package:flutter_mobile/udemy_max/chat_app/screens/chat.dart';
+import 'package:flutter_mobile/udemy_max/chat_app/screens/splash.dart';
 
 class MainChatApp extends StatelessWidget {
   const MainChatApp({super.key});
@@ -7,12 +10,25 @@ class MainChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FlutterChat',
-      theme: ThemeData().copyWith(
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 63, 17, 177)),
-      ),
-      home: const AuthScreen()
-    );
+        title: 'FlutterChat',
+        theme: ThemeData().copyWith(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 63, 17, 177)),
+        ),
+        home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: ((context, snapshot) {
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const SplashScreen();
+            }
+
+            if (snapshot.hasData) {
+              return const ChatScreen();
+            }
+
+            return const AuthScreen();
+          }),
+        ));
   }
 }
---
+++ lib/udemy_max/chat_app/screens/chat.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('FlutterChat'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.exit_to_app),
+              color: Theme.of(context).colorScheme.primary,
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+            ),
+          ],
+        ),
+        body: const Center(
+          child: Text('Logged in!'),
+        ));
+  }
+}
