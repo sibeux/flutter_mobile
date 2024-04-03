6cb15cdee1da7ec674e934279e00c1bb7a74b6a9
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-04-04 01:54:41 +0700

    Add Firebase configuration files and implement push notifications

--- lib/udemy_max/chat_app/screens/chat.dart
+++ lib/udemy_max/chat_app/screens/chat.dart
@@ -1,10 +1,30 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobile/udemy_max/chat_app/widgets/chat_messages.dart';
 import 'package:flutter_mobile/udemy_max/chat_app/widgets/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+
+    fcm.subscribeToTopic('chat');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    setupPushNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
