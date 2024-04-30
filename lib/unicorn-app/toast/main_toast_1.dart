526eaeb4d23aafa6eaecb9c5cb8800bb68b90ede
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-04-30 19:37:39 +0700

    Refactor MainToastOneApp and add toast notification feature

--- lib/unicorn-app/toast/main_toast_1.dart
+++ lib/unicorn-app/toast/main_toast_1.dart
@@ -1,7 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-class MainToastOneApp extends StatelessWidget {
+GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+class MainToastOneApp extends StatefulWidget {
   const MainToastOneApp({super.key});
+
+  @override
+  State<MainToastOneApp> createState() => _MainToastOneAppState();
+}
+
+class _MainToastOneAppState extends State<MainToastOneApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Toast Notification',
+      builder: FToastBuilder(),
+      navigatorKey: navigatorKey,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -9,14 +36,27 @@ class MainToastOneApp extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Toast Notification'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Show Toast 1'),
-          ),
-        ],
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Fluttertoast.showToast(
+                  msg: 'This is a toast notification',
+                  toastLength: Toast.LENGTH_SHORT,
+                  gravity: ToastGravity.BOTTOM,
+                  timeInSecForIosWeb: 1,
+                  backgroundColor: Colors.red,
+                  textColor: Colors.white,
+                  fontSize: 12.0,
+                );
+              },
+              child: const Text('Show Toast'),
+            ),
+          ],
+        ),
       ),
     );
   }
