3db5677168dd16b5eb9e362794f004d1a1d967fa
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-05-17 16:03:09 +0700

    Refactor MainChatApp and update theme color

--- lib/udemy_max/chat_app/main_chat_app.dart
+++ lib/udemy_max/chat_app/main_chat_app.dart
@@ -10,25 +10,25 @@ class MainChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'FlutterChat',
-        theme: ThemeData().copyWith(
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: const Color.fromARGB(255, 63, 17, 177)),
-        ),
-        home: StreamBuilder(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: ((context, snapshot) {
+      title: 'FlutterChat',
+      theme: ThemeData().copyWith(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 63, 17, 177)),
+      ),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: ((context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
 
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const SplashScreen();
-            }
+          if (snapshot.hasData) {
+            return const ChatScreen();
+          }
 
-            if (snapshot.hasData) {
-              return const ChatScreen();
-            }
-
-            return const AuthScreen();
-          }),
-        ));
+          return const AuthScreen();
+        }),
+      ),
+    );
   }
 }
