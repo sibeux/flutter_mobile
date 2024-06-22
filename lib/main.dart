f93ebaf09aba350eab7dcd151a1e2112750dab76
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-06-23 02:43:35 +0700

    feat: Add login functionality
    This commit adds the login functionality to the app by introducing a new `LoginApp` widget and related components. The `LoginApp` widget allows users to enter their email and password, sends a POST request to the server for authentication, and handles the response accordingly. Upon successful login, the user is redirected to the `HomeScreen`. This feature enhances the app by providing a secure login system for users.

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ import 'package:flutter_mobile/unicorn-app/how-to-play-music/screen/music_screen
 import 'package:flutter_mobile/unicorn-app/just-audio-background/main_just_audio_background.dart';
 import 'package:flutter_mobile/unicorn-app/lazy-load-scroll/main_lazy_1.dart';
 import 'package:flutter_mobile/unicorn-app/lazy-load-scroll/main_lazy_2.dart';
+import 'package:flutter_mobile/unicorn-app/login-session/login.dart';
 import 'package:flutter_mobile/unicorn-app/music-player-app/main_music_app.dart';
 import 'package:flutter_mobile/unicorn-app/toast/main_toast_1.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -59,7 +60,8 @@ Future<void> main(List<String> args) async {
     // runApp(const MainChatApp());
     // runApp(const MainLazyOneApp());
     // runApp(const MainLazyTwoApp());
-    runApp(const MainToastOneApp());
+    // runApp(const MainToastOneApp());
+    runApp(LoginApp());
     // runApp(const ProviderScope(child: MainMusicPlayerApp()));
     // runApp(const ProviderScope(child: MainJustAudioBackgroundApp()));
   });
